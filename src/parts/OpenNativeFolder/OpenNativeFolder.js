5f682486b3434c28c7b2952e82d4445dd44bda7a
Le Vivilet <levivlet@pm.me>
2024-07-06 22:26:04 +0200

    feature: lazyload open module so that other commands still work if open package is not installed

--- src/parts/OpenNativeFolder/OpenNativeFolder.js
+++ src/parts/OpenNativeFolder/OpenNativeFolder.js
@@ -1,8 +1,8 @@
-import open from 'open'
 import { VError } from '../VError/VError.js'
 
 export const openFolder = async (path) => {
   try {
+    const { default: open } = await import('open')
     await open(path)
   } catch (error) {
     throw new VError(error, `Failed to open ${path}`)

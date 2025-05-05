341d08168c4bd62e6c2404681016c491aba8660d
Le Vivilet <levivlet@pm.me>
2025-05-06 01:07:47 +0200

    build

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -74,7 +74,7 @@ const getWsVersion = async () => {
   const parsed = JSON.parse(stdout)
   const wsVersion =
     parsed.dependencies['@lvce-editor/rpc'].dependencies['@lvce-editor/ipc']
-      .dependencies['@lvce-editor/web-socket-server'].dependencies['ws'].version
+      .dependencies['ws'].version
   return wsVersion
 }
 

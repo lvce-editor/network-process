f9a67987ee0062bb8a96fb039132baea9a4f5a43
Le Vivilet <levivilet@pm.me>
2026-07-10 20:52:18 +0000

    fix(lint): suppress no-misused-promises crash in network-process test

--- packages/network-process/test/Download.test.ts
+++ packages/network-process/test/Download.test.ts
@@ -78,5 +78,6 @@ test('download', async () => {
   streamPromises.pipeline.mockImplementation(() => {})
   await Download.download('https://example/file.txt', '/test/folder')
   expect(fsPromises.mkdir).toHaveBeenCalled()
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   expect(streamPromises.pipeline).toHaveBeenCalled()
 })

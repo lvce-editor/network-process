c32ec8c4b71874b1f99d6c85cb69b326ca8af9f8
Le Vivilet <levivilet@pm.me>
2026-08-01 15:26:52 +0000

    feature: update eslint dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
 
-export default [
+export default defineConfig([
   ...config.default,
   ...config.recommendedNode,
   {
@@ -26,4 +27,4 @@ export default [
       'jest/no-restricted-jest-methods': 'off',
     },
   },
-]
+])
